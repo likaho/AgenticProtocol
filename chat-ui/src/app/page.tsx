9bc9bdc99b292d17562b6c1ae5124b7a62298e4c
likaho <kaho.li@gmail.com>
2024-07-03 07:04:54 +0100

    Added projects

---
+++ chat-ui/src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { FullPageChat } from "flowise-embed-react";
+
+export default function Home() {
+  return (
+    <FullPageChat
+    chatflowid="b76ff49c-870e-4c2c-a447-dc598ea8b4b9"
+    apiHost="http://localhost:3032"/>
+  );
+} 
