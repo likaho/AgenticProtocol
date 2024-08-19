4b7b77a854062d421a949c204dea366b98fc84e5
Kaho Li <kaho.li@gmail.com>
2024-08-19 11:09:05 +0100

    Add new columns in the modify script

--- core/packages/server/src/database/migrations/sqlite/1693920824108-ModifyChatFlow.ts
+++ core/packages/server/src/database/migrations/sqlite/1693920824108-ModifyChatFlow.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class ModifyChatFlow1693920824108 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "temp_chat_flow" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "flowData" text NOT NULL, "deployed" boolean, "isPublic" boolean, "apikeyid" varchar, "chatbotConfig" text, "ownerAddress" varchar, "createdDate" datetime NOT NULL DEFAULT (datetime('now')), "updatedDate" datetime NOT NULL DEFAULT (datetime('now')));`
+            `CREATE TABLE "temp_chat_flow" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "flowData" text NOT NULL, "deployed" boolean, "isPublic" boolean, "apikeyid" varchar, "chatbotConfig" text, "ownerAddress" varchar, "tokenId" varchar, "nftAddress" varchar, "isPublished" boolean, "usageCount" integer, "gas" integer, "description" text, "createdDate" datetime NOT NULL DEFAULT (datetime('now')), "updatedDate" datetime NOT NULL DEFAULT (datetime('now')));`
         )
         await queryRunner.query(
-            `INSERT INTO "temp_chat_flow" ("id", "name", "flowData", "deployed", "isPublic", "apikeyid", "chatbotConfig", "ownerAddress", "createdDate", "updatedDate") SELECT "id", "name", "flowData", "deployed", "isPublic", "apikeyid", "chatbotConfig", "ownerAddress", "createdDate", "updatedDate" FROM "chat_flow";`
+            `INSERT INTO "temp_chat_flow" ("id", "name", "flowData", "deployed", "isPublic", "apikeyid", "chatbotConfig", "ownerAddress", "tokenId", "nftAddress", "isPublished", "usageCount", "gas", "description", "createdDate", "updatedDate") SELECT "id", "name", "flowData", "deployed", "isPublic", "apikeyid", "chatbotConfig", "ownerAddress", "tokenId", "nftAddress", "isPublished", "usageCount", "gas", "description", "createdDate", "updatedDate" FROM "chat_flow";`
         )
         await queryRunner.query(`DROP TABLE chat_flow;`)
         await queryRunner.query(`ALTER TABLE temp_chat_flow RENAME TO chat_flow;`)
